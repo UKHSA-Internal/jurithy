d10631d4340e457882764d0f4ac5525ab7652119
Olisaeloka Nsonwu <47105194+OlisaNsonwu@users.noreply.github.com>
2025-07-14 15:59:02 +0100

    0.1.0.9018

--- R/aggregate_los.R
+++ R/aggregate_los.R
@@ -4,11 +4,10 @@
 #'
 #' @param start_date Date. Event start date
 #' @param end_date Date. Event end date
-#' @param start_date_point Character|Date. Start point to begin aggregation.
-#' Acceptable options are "start_of_day", "start_of_month", "start_of_quarter"
-#' or "start_of_year". Alternatively, a specific date can be provided.
+#' @param los_range NULL|Date. A date range of the period to cover in the aggregation.
+#' If \code{NULL} (default), the rane of \code{start_date} and \code{start_date} is used.
 #' @param level Character. Desired level. Passed to 'by' in \code{\link{seq}}.
-#' Options include all accepted in \code{\link[=seq]{seq(..., by = XX)}}
+#' Options include all accepted in \code{\link[=seq.POSIXt]{seq.POSIXt(..., by = XX)}}
 #' @param strata Atomic. Population strata
 #' @param silent Logical. Print (or not) current iteration of loop.
 #' @param count_id Atomic. If provided the unique count of \code{count_id}
@@ -17,79 +16,92 @@
 #' per iteration (batch) is included
 #' @param fill Logical. If TRUE, place holders are included if
 #' there is no occurrence of the event within the batch of the loop.
-#' @param units Character. Desired unit for level. Passed to 'by' in \code{\link{seq}}.
-#' Options include all accepted in \code{\link[=seq]{seq(..., by = XX)}}
+#' @param units Character. Desired unit for LOS. Passed to \code{\link{difftime}}.
+#' Options include all accepted in \code{\link{difftime}}s
 #'
-#' @return A data frame
-#'
+#' @return A data frame with the following columns
+#' \itemize{
+#' \item \code{strata} - See \code{strata} argument.
+#' \item \code{days_origin_0} - Total length of stay per \code{strata} when the first day in each period was taken day 0.
+#' \item \code{days_origin_1} - Total length of stay per \code{strata} when the first day in each period was taken day 1.
+#' \item \code{unique.count_id} - Unique count of \code{count.id} per per \code{strata}.
+#' \item \code{unique.event_id} - Unique count of \code{event_id} per per \code{strata}.
+#' \item \code{batch} - Batch of length of stay per \code{strata} and \code{level}
+#' \item \code{start_date} - Start date of \code{batch}
+#' \item \code{end_date} - End date of \code{batch}
+#' }
 #' @examples
+#' # Example of dated events
 #' dfr <- data.frame(
 #'   start_date = as.Date(c('2024-05-28', '2024-05-05', '2024-06-01')),
 #'   end_date = as.Date(c('2024-06-05', '2024-05-11', '2024-06-20')),
 #'   strata = c('A', 'A', 'B'))
 #'
-#' dfr$duration <- difftime(dfr$end_date, dfr$start_date, 'days')
+#' # Length of stay across the entire period
+#'  dfr$duration <- difftime(dfr$end_date, dfr$start_date, 'days')
+#'  dfr
 #'
-#' dfr
+#'  # Aggregate to a annual level
+#'  aggregate_los(
+#'    start_date = dfr$start_date,
+#'    end_date = dfr$end_date,
+#'    level = 'year',
+#'    strata = dfr$strata
+#'  )
 #'
-#' aggregate_los(
-#'   start_date = dfr$start_date,
-#'   end_date = dfr$end_date
-#' )
+#'  # Aggregate to a monthly level
+#'  aggregate_los(
+#'    start_date = dfr$start_date,
+#'    end_date = dfr$end_date,
+#'    level = 'month',
+#'    strata = dfr$strata
+#'  )
 #'
-#' aggregate_los(
-#'   start_date = dfr$start_date,
-#'   end_date = dfr$end_date,
-#'   level = 'month'
-#' )
+#'  # Cover a range of wider of dates. Use `fill` for empty rows
+#'  aggregate_los(
+#'    start_date = dfr$start_date,
+#'    end_date = dfr$end_date,
+#'    level = 'month',
+#'    strata = dfr$strata,
+#'    los_range =  c('2024-01-01', '2024-12-01')
+#'  )
 #'
-#' aggregate_los(
-#'   start_date = dfr$start_date,
-#'   end_date = dfr$end_date,
-#'   level = 'month',
-#'   strata = dfr$strata
-#' )
+#'  aggregate_los(
+#'    start_date = dfr$start_date,
+#'    end_date = dfr$end_date,
+#'    level = 'month',
+#'    strata = dfr$strata,
+#'    los_range =  c('2024-01-01', '2024-12-01'),
+#'    fill = TRUE
+#'  )
 #'
-#' aggregate_los(
-#'   start_date = dfr$start_date,
-#'   end_date = dfr$end_date,
-#'   level = 'month',
-#'   strata = dfr$strata,
-#'   start_date_point = 'start_of_month'
-#' )
+#'  # Return LOS is different units of time
+#'  aggregate_los(
+#'    start_date = dfr$start_date,
+#'    end_date = dfr$end_date,
+#'    level = 'month',
+#'    strata = dfr$strata,
+#'    los_range =  c('2024-01-01', '2024-12-01'),
+#'    units = 'weeks'
+#'  )
 #'
-#' aggregate_los(
-#'   start_date = dfr$start_date,
-#'   end_date = dfr$end_date,
-#'   level = 'month',
-#'   strata = dfr$strata,
-#'   start_date_point = 'start_of_month',
-#'   units = 'weeks'
-#' )
 #'
-#' aggregate_los(
-#'   start_date = as.POSIXct(dfr$start_date),
-#'   end_date = as.POSIXct(dfr$end_date),
-#'   level = 'month',
-#'   strata = dfr$strata,
-#'   start_date_point = 'start_of_month',
-#'   units = 'mins'
-#' )
-#'
-#' aggregate_los(
-#'   start_date = dfr$start_date,
-#'   end_date = dfr$end_date,
-#'   level = 'day',
-#'   strata = dfr$strata,
-#'   start_date_point = 'start_of_month',
-#'   units = 'days'
-#' )
+#'  # Return LOS is different units of time
+#'  aggregate_los(
+#'    start_date = dfr$start_date,
+#'    end_date = dfr$end_date,
+#'    level = '2 years',
+#'    strata = dfr$strata,
+#'    los_range =  c('2020-01-01', '2025-12-01'),
+#'    units = 'weeks',
+#'    fill = TRUE
+#'  )
 #'
 #' @export
 aggregate_los <- function(
     start_date, end_date, level = diff(range(start_date, end_date)),
-    strata = NULL, count_id = NULL, event_id = NULL, start_date_point = 'start_of_day',
-    fill = FALSE, silent = TRUE, units = 'days'){
+    strata = NULL, count_id = NULL, event_id = NULL,
+    fill = FALSE, silent = TRUE, units = 'days', los_range = NULL){
 
   dfr <- list(
     start_date = start_date,
@@ -99,46 +111,20 @@ aggregate_los <- function(
     strata = strata
   )
 
-  if((!inherits(dfr$start_date, c("POSIXct", "POSIXt")) |
-     !inherits(dfr$end_date, c("POSIXct", "POSIXt")) |
-     inherits(start_date_point, c("Date"))) & !units %in% c("days", "weeks")){
-    stop("`start_date`, `end_date` and `start_date_point` must be `POSIXct` or `POSIXt` objects if `units` is not 'days' or 'weeks'")
-  }
-
-  if((inherits(dfr$start_date, c("POSIXct", "POSIXt")) |
-      inherits(dfr$end_date, c("POSIXct", "POSIXt")) |
-      inherits(start_date_point, c("POSIXct", "POSIXt"))) & units %in% c("days", "weeks")){
-    stop("`start_date`, `end_date` and `start_date_point` must be `Date` objects if `units` is not 'days' or 'weeks'")
-  }
-
   dfr$los_period <- difftime(dfr$start_date, dfr$end_date, units = units)
 
-  ll_dt_a <- min(dfr$start_date)
-  ll_dt_z <- max(dfr$end_date)
-
-  if(inherits(start_date_point, c("POSIXct", "POSIXt", "Date"))){
-    ll_dt_a <- start_date_point
-  }else if(start_date_point == 'start_of_day'){
-
-  }else if(start_date_point == 'start_of_quarter'){
-    ll_dt_a <- as.Date(paste0(
-      substr(period_cd(ll_dt_a, 'cq'), 1, 4), '-',
-      stringr::str_pad(seq(1,12, by = 3)[
-        as.numeric(substr(period_cd(ll_dt_a, 'cq'), 5, 5))
-      ],  width=2, pad="0"), '-01'))
-  }else if(start_date_point == 'start_of_year'){
-    ll_dt_a <- as.Date(paste0(period_cd(ll_dt_a, 'cy'), '-01-01'))
-  }else if(start_date_point == 'start_of_month'){
-    ll_dt_a <- as.Date(paste0(
-      substr(period_cd(ll_dt_a, 'cm'), 1, 4), '-',
-      substr(period_cd(ll_dt_a, 'cm'), 5, 6), '-01'))
+  dt_range <- c(dfr$start_date, dfr$end_date)
+  if(!is.null(los_range)){
+    dt_range <- c(dt_range, los_range)
   }
+  ll_dt_a <- range(dt_range)
+
   if(!units %in% c("days", "weeks")){
     ll_dt_a <- as.POSIXct(ll_dt_a)
   }
 
-  bd_a <- seq(from = ll_dt_a, to = ll_dt_z, by = level)
-  bd_a <- unique(c(bd_a, ll_dt_z))
+  bd_a <- seq(from = ll_dt_a[1], to = ll_dt_a[2], by = level)
+  bd_a <- unique(c(bd_a, ll_dt_a[2]))
   bd_z <- bys_lead(bd_a)
 
   if(
@@ -160,7 +146,8 @@ aggregate_los <- function(
     bd_z[indx] <- bd_z[indx]-1
   }
 
-
+  st <- unique(dfr$strata)
+  st_len <- length(st)
   bd_tot <- length(bd_a)
   los_lst <- list()
   for(i in seq_len(bd_tot)){
@@ -177,10 +164,11 @@ aggregate_los <- function(
     if(length(dfr4[[1]]) == 0){
       if(fill){
         los_lst[[i]] <- list(
-          strata = NA, overnight = 0, days = 0,
-          unique_event_id = 0, unique_count_id = 0,
-          batch = i, start_date = bd_a[i],
-          end_date = bd_z[i])
+          strata = st, los_origin_0 = rep(0, st_len),
+          los_origin_1 = rep(0, st_len),
+          unique.event_id = rep(0, st_len), unique.count_id = rep(0, st_len),
+          batch = rep(i, st_len), start_date = rep(bd_a[i], st_len),
+          end_date = rep(bd_z[i], st_len))
       }
       next
     }
@@ -192,12 +180,8 @@ aggregate_los <- function(
     dfr4$end_date[lgk_r] <- bd_z[i]
     dfr4$tmp.los_a <-
       difftime(dfr4$end_date, dfr4$start_date, units = units)
-
-    if(any(grep('sec|day', units))){
-      dfr4$tmp.los_b <- dfr4$tmp.los_a + 1
-    }else{
-      dfr4$tmp.los_b <- rep(NA, length(dfr4$tmp.los_a))
-    }
+    dfr4$tmp.los_b <-
+      difftime(dfr4$end_date + lubridate::ddays(1), dfr4$start_date, units = units)
 
     # Adding 1-day diff (overnight stay) that's lost due to split from a previous month
     dfr4$tmp.los_a[lgk_l] <- dfr4$tmp.los_a[lgk_l] + 1
@@ -210,13 +194,13 @@ aggregate_los <- function(
       dfr4$cmb <- combi(dfr4$strata)
     }
 
-    dfr4$diff <- bys_sum(by = dfr4$cmb, as.numeric(dfr4$tmp.los_a))
-    dfr4$duration <- bys_sum(by = dfr4$cmb, as.numeric(dfr4$tmp.los_b), na.rm = FALSE)
+    dfr4$los_origin_0 <- bys_sum(by = dfr4$cmb, as.numeric(dfr4$tmp.los_a))
+    dfr4$los_origin_1 <- bys_sum(by = dfr4$cmb, as.numeric(dfr4$tmp.los_b), na.rm = FALSE)
     dfr4$unique.event_id <- bys_sum(by = dfr4$cmb, as.numeric(dfr4$unique.event_id), na.rm = TRUE)
     dfr4$unique.count_id <- bys_sum(by = dfr4$cmb, as.numeric(dfr4$unique.count_id), na.rm = TRUE)
 
     lgk <- which(!duplicated(dfr4$cmb))
-    dfr4 <- lapply(dfr4[c("strata", "diff", "duration",
+    dfr4 <- lapply(dfr4[c("strata", "los_origin_0", "los_origin_1",
                           "unique.count_id", 'unique.event_id')],
                    function (x) x[lgk])
 
@@ -228,9 +212,13 @@ aggregate_los <- function(
     los_lst[[i]] <- as.data.frame(dfr4)
     rm(dfr4)
   }
+
   los_lst <- do.call("rbind", los_lst)
+  los_lst <- los_lst[order(los_lst$strata, los_lst$batch),]
+
+  los_lst$unique.count_id[is.na(los_lst$unique.count_id)] <- 0
+  los_lst$unique.event_id[is.na(los_lst$unique.event_id)] <- 0
 
   return(los_lst)
 }
-
 
