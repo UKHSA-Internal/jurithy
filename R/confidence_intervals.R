27ba3e334b9869665845f8bac7a207dc55554dc1
OlisaNsonwu <47105194+OlisaNsonwu@users.noreply.github.com>
2025-10-14 10:42:07 +0100

    0.1.0.9019

--- R/confidence_intervals.R
+++ R/confidence_intervals.R
@@ -1,11 +1,11 @@
 #' @name ci
 #' @title Confidence intervals for a difference between two proportions
 #'
-#' @param t1 Proportion 1
-#' @param r1 Incidence rate 1
-#' @param n1 Sample size 1
-#' @param t2 Proportion 2
-#' @param r2 Incidence rate 2
+#' @param t1 Population size 1
+#' @param r1 Rate per population at risk 1
+#' @param n1 Sample 1
+#' @param t2 Population size 2
+#' @param r2 Rate per population at risk 2
 #' @param n2 Sample size 2
 #' @param sig_level Significance level
 #'
@@ -67,13 +67,15 @@ ci_rate_ratio <- function(r1, n1, r2, n2, sig_level = .95){
   z <- stats::qnorm(1 - alpha)
 
   rr <- r1/r2
-  z2 <- base::abs(rr/se)
+  log_rr <- base::log(rr)
+
+  z2 <- base::abs(log_rr/se)
   p <- base::exp(-0.717 * z2 - 0.416 * z2^2)
   list(rate_ratio = rr,
        se = se,
        p_value = p,
-       lo = base::exp(base::log(rr) - (z * se)),
-       hi = base::exp(base::log(rr) + (z * se)))
+       lo = base::exp(log_rr - (z * se)),
+       hi = base::exp(log_rr + (z * se)))
 }
 
 #' @rdname ci
@@ -92,13 +94,14 @@ ci_risk_ratio <- function(n1, t1, n2, t2, sig_level = .95){
   if(!is.atomic(sig_level)) stop("`sig_level` must be an `atomic` vector!")
   if(!sig_level >= 0 && sig_level<= 1) stop("`sig_level` must be beyween 0 and  1!")
 
+  se <- base::sqrt(((1/n1)-(1/t1)) + ((1/n2)-(1/t2)))
   alpha <- 0.5 * (1 - sig_level)
   z <- stats::qnorm(1 - alpha)
 
   rr <- (n1/t1)/(n2/t2)
   log_rr <- base::log(rr)
-  se <- base::sqrt(((1/(n1))-(1/(t1))) + ((1/(n2))-(1/(t2))))
-  z2 <- base::abs(rr/se)
+
+  z2 <- base::abs(log(rr)/se)
   p <- base::exp(-0.717 * z2 - 0.416 * z2^2)
   lci <- base::exp(log_rr - (z * se))
   uci <- base::exp(log_rr + (z * se))
